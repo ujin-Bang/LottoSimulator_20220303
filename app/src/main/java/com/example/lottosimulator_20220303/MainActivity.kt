941eecfbcdff383a060c61ee047c0f106187ad98
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-03 14:45:25 +0900

    2등 판정 - 보너스 번호 검사 로직 추가

--- app/src/main/java/com/example/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/example/lottosimulator_20220303/MainActivity.kt
@@ -122,7 +122,15 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "1등입니다.", Toast.LENGTH_SHORT).show()
             }
             5 -> {
-                Toast.makeText(this, "임시 3등입니다.", Toast.LENGTH_SHORT).show()
+
+//                보너스 번호를 맞췄는지? => 보너스 번호가 내 번호 목록에 들어있나?
+                if(mMyNumbers.contains(mBonusNum)){
+                    Toast.makeText(this, "2등입니다.", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "3등입니다.", Toast.LENGTH_SHORT).show()
+
+                }
 
             }
             4 -> {
