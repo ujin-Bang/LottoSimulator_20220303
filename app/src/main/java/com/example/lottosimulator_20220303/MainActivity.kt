915da9f33cdc1f55f01515ddab4a6beed43dffc3
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-03 15:33:54 +0900

    1천만원이 안된 상황에서는 다시 자기 자신을 실행하는 Runnable 생성

--- app/src/main/java/com/example/lottosimulator_20220303/MainActivity.kt
+++ app/src/main/java/com/example/lottosimulator_20220303/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.lottosimulator_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
@@ -34,7 +36,36 @@ class MainActivity : AppCompatActivity() {
     var rankCount4 = 0
     var rankCount5 = 0
     var rankCountFail = 0
+    
+//    Handler로 쓰레드에 할일 할당. (postDelayde - 일정 시간 지난 뒤에 할 일 할당)
+    lateinit var mHandler: Handler
+    
+//    핸들러가 반복 실행할 코드(로또 다시 구매)를, 인터페이스를 이용해서 변수로 저장.
 
+    val buyLottoRunnable = object : Runnable {
+        
+        override fun run() {
+            
+//            물려받은 추상 메쏘드 구현
+//            할 일이 어떤건지 적는 함수
+            
+//            쓴 돈이 1천만원이 안된다면 추가 구매
+            if(mUsedMoney <= 100000) {
+                buyLotto()
+                
+//                핸들러에게 다음 할일로 지금한 이 코드를 다시 등록시킨다.
+                mHandler.post(this)
+            }
+            
+//            그렇지 않다면, 할일 정지
+            else {
+                Toast.makeText(this@MainActivity, "자동 구매가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+            
+        }
+
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,15 +78,9 @@ class MainActivity : AppCompatActivity() {
        btnAutoBuy.setOnClickListener {
 
 //           처음 눌리면 > 반복 구매 시작 > 1천만원 사용할 때까지 반복
-
-//           단순 반복을 돌리면 > 반복 속도가 너무 빨라서 UI가 멈춘 것처럼 보인다.
-           while (true) {
-               buyLotto()
-               if (mUsedMoney >= 10000000){
-                   break
-               }
-           }
-
+//          1회 로또 구매 명령 > 완료되면 다시 1회 로또 구매 > 연속 클릭을 자동으로 하는 느낌으로
+           
+           
 //           반복 구매중에 눌리면 > 반복 종료
        }
 
@@ -200,6 +225,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setValues(){
+        
+//        반복을 담당할 핸들러를 생성
+        mHandler = Handler(Looper.getMainLooper())
 
         mWinNumTextViewList.add(txtWinNum01)
         mWinNumTextViewList.add(txtWinNum02)
